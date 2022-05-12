2f3ccd60267648659adc40f92d930b29c2c967d0
Brayden Bos <32762@ma-web.nl>
2022-05-12 11:51:50 +0200

    collision

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -4,33 +4,29 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
-    public float speed;
-    // Start is called before the first frame update
+    public float movementSpeed;
     void Start()
     {
-        
-    }
 
-    // Update is called once per frame
+    }
     void Update()
     {
         if (Input.GetKey(KeyCode.W))
         {
-                transform.position += new Vector3(0, 0.1f * Time.deltaTime * speed, 0);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.position -= new Vector3(0, 0.1f * Time.deltaTime * speed, 0);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.position -= new Vector3(0.1f * Time.deltaTime * speed, 0, 0);
+            transform.position += new Vector3(0, movementSpeed, 0) * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.D))
         {
-            transform.position += new Vector3(0.1f * Time.deltaTime * speed, 0, 0);
+            transform.position += new Vector3(movementSpeed,0, 0) * Time.deltaTime;
         }
-
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += new Vector3(0, -movementSpeed, 0) * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(-movementSpeed, 0, 0) * Time.deltaTime;
+        }
 
     }
 }
