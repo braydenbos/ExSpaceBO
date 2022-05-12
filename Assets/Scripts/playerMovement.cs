c8abe0f71fe68285b1702ad04e2da4d6488f2bbf
Brayden Bos <32762@ma-web.nl>
2022-05-12 23:26:50 +0200

    sprintbar

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -1,16 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class playerMovement : MonoBehaviour
 {
     public float movementSpeed;
+    private float originalSpeed;
+    private float sprint = 20;
+    public float speedmultiplier;
+    private bool isSprinting = false;
+    public RectTransform sprintbar;
     void Start()
     {
-
+        originalSpeed = movementSpeed;
     }
     void Update()
     {
+        sprintbar.sizeDelta = new Vector2(sprint, sprintbar.sizeDelta.y);
+        // Sprinting
+        print(movementSpeed);
+        if (Input.GetKeyDown(KeyCode.LeftShift) && sprint >= 20 && !isSprinting)
+        {
+            movementSpeed *= speedmultiplier;
+            isSprinting = true;
+        }
+        if(Input.GetKey(KeyCode.LeftShift) && isSprinting)
+        {
+            sprint -= 10 * Time.deltaTime;
+        }
+        if (!isSprinting && sprint < 20)
+        {
+            sprint += 8 * Time.deltaTime;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift) || sprint <= 0)
+        {
+            movementSpeed = originalSpeed;
+            isSprinting = false;
+        }
+
+        // Walking
         if (Input.GetKey(KeyCode.W))
         {
             transform.position += new Vector3(0, movementSpeed, 0) * Time.deltaTime;
@@ -27,6 +56,5 @@ public class playerMovement : MonoBehaviour
         {
             transform.position += new Vector3(-movementSpeed, 0, 0) * Time.deltaTime;
         }
-
     }
 }
