95eb0b22bb9b938a5fd3dc7675941c1f483e9937
Tomm9000 <90690902+Tomm9000@users.noreply.github.com>
2022-06-28 21:46:37 +0200

    Halve Merge errors gefixed

--- Assets/Scripts/endScreen.cs
+++ Assets/Scripts/endScreen.cs
@@ -1,15 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-
 
 public class endScreen : MonoBehaviour
 {
-    private float Timer;
-    public Image forNow;
+    public CanvasGroup forNowGroup;
+    public CanvasGroup youSurvivedGroups;
+    public CanvasGroup buttonGroup;
 
+    public bool toFade;
+    void Start()
+    {
+        forNowGroup.alpha = 0f;
+        youSurvivedGroups.alpha = 0f;
+        buttonGroup.alpha = 0f;
+        toFade = true;
+    }
     void Update()
     {
+        if (toFade)
+        {
+            if(forNowGroup.alpha < 1f || youSurvivedGroups.alpha < 1f)
+            {
+                youSurvivedGroups.alpha += Time.deltaTime / 8;
+                forNowGroup.alpha += Time.deltaTime / 9;
+                buttonGroup.alpha += Time.deltaTime / 10;
+                if (forNowGroup.alpha >= 1f)
+                {
+                    toFade = false;
+                }
+            }
+        }
     }
 }
